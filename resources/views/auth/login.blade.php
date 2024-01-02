5fbfb830c5f7ccc2e6e2055b705f0b901890c201
FigoRazzan <109186283+FigoRazzan@users.noreply.github.com>
2024-01-03 00:37:13 +0700

    Sudah Ditambah Gambar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,34 +8,35 @@
 
 <div class="w-100">
 
+
     <main class="form-signin w-100 m-auto">
         <form method="POST" action="{{ route('auth.login') }}" id="login-form">
-            @csrf
             <h1 class="h3 mb-3 fw-normal">Silahkan masuk untuk absensi</h1>
 
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInputEmail" name="email"
-                    placeholder="name@example.com">
+                <input type="email" class="form-control" id="floatingInputEmail" name="email" placeholder="name@example.com">
                 <label for="floatingInputEmail">Email address</label>
             </div>
+
             <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" name="password"
-                    placeholder="Password">
+                <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
                 <label for="floatingPassword">Password</label>
             </div>
 
             <div class="form-check mb-3">
                 <input class="form-check-input" type="checkbox" name="remember" id="flexCheckRemember">
-                <label class="form-check-label" for="flexCheckRemember">
-                    Ingatkan Saya di Perangkat ini
-                </label>
+                <label class="form-check-label" for="flexCheckRemember">Ingatkan Saya di Perangkat ini</label>
             </div>
 
             <button class="w-100 btn btn-primary" type="submit" id="login-form-button">Masuk</button>
             <p class="mt-5 mb-3 text-muted">&copy; 2023</p>
         </form>
     </main>
-
+</div>
+<div class="gambar">
+        <img src="{{ asset('css/Foto/hormat.png') }}" alt="Gambar 1">
+        <img src="{{ asset('css/Foto/Lambang_Kota_Serang.png') }}" alt="Gambar 2">
+        <img src="{{ asset('css/Foto/paskibraka.png') }}" alt="Gambar 3">
 </div>
 @endsection
 
